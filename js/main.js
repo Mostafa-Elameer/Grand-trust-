8eeb379479e9e00e1f56b2a7a5589d320a9e25a9
mostafa <139650173+Mostafa-Elameer@users.noreply.github.com>
2024-10-23 13:26:33 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// window.alert("hi")
+
+
+var typed = new Typed("#text-", {
+    strings: ["سكنك واستثماراتك في مكان واحد", "عقارات المستقبل", "مصداقيه وثقه"],
+    typeSpeed: 60,
+    loop: true,
+    loopCount: Infinity,
+    backSpeed: 30,
+});
