45a0865be3816a894dca91c274557d657bee8ac5
Amal Abdelhakim <148907902+M00SY@users.noreply.github.com>
2024-10-23 16:48:59 +0300

    Test home page

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 // window.alert("hi")
 
-
 var typed = new Typed("#text-", {
-    strings: ["سكنك واستثماراتك في مكان واحد", "عقارات المستقبل", "مصداقيه وثقه"],
-    typeSpeed: 60,
-    loop: true,
-    loopCount: Infinity,
-    backSpeed: 30,
+  strings: [
+    " سكنك واستثماراتك في مكان واحد",
+    " عقارات المستقبل",
+    " مصداقيه وثقه",
+  ],
+  typeSpeed: 70,
+  loop: true,
+  loopCount: Infinity,
+  backSpeed: 30,
 });
