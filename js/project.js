d71eaf32541065a4916822eee804e8e23743111b
Amal Abdelhakim <148907902+M00SY@users.noreply.github.com>
2024-10-25 20:16:32 +0300

    update js project

--- js/project.js
+++ js/project.js
@@ -1,10 +1,9 @@
-
 var typed = new Typed("#text", {
-    strings: ["نظرا لموقعه الاستراتيجي وخدماته المميزة", "إلا نه يعد ميزة استثمارية هامة ايضا ", "مستقبل أفضل وحياة أرقي"],
-    typeSpeed: 60,
-    loop: true,
-    loopCount: Infinity,
-    backSpeed: 30,
+  strings: [
+    "بفضل موقعه الاستراتيجي وخدماته المميزة، يعتبر فرصة استثمارية هامة، ويوفر لك مستقبلًا أفضل وحياة أرقى.",
+  ],
+  typeSpeed: 60,
+  loop: true,
+  loopCount: Infinity,
+  backSpeed: 30,
 });
-
-  
